61c7a403fa630d19edc34038ca08ac3c1e8f9391
prasert73 <115928283+prasert73@users.noreply.github.com>
2023-01-08 10:43:11 +0700

    completed

--- index.js
+++ index.js
@@ -24,6 +24,11 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', function (req, res){
+  const {"x-forwarded-for": ip, "accept-language" : language, "user-agent": software } = req.headers;
+  res.json({ipaddress: ip, language: language, software: software});
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
